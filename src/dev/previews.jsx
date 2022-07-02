1f7e6acf8336f490a07e908cfebf186bdc2d48c2
alexandrufako <alexandru.fako@gmail.com>
2022-07-02 20:39:26 +0300

    some changes

--- src/dev/previews.jsx
+++ src/dev/previews.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Previews } from '@react-buddy/ide-toolbox';
+import { ComponentPreview, Previews } from '@react-buddy/ide-toolbox';
 import { PaletteTree } from './palette';
+import AboutPage from '../pages/about';
+import Header from '../components/header/header';
 
 const ComponentPreviews = () => {
   return (
     <Previews palette={<PaletteTree />}>
+      <ComponentPreview path='/AboutPage'>
+        <AboutPage />
+      </ComponentPreview>
+      <ComponentPreview path='/Header'>
+        <Header />
+      </ComponentPreview>
     </Previews>
   );
 };
